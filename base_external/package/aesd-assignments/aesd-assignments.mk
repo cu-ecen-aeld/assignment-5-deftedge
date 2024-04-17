0398dace3bd41ac5afc14471a47629fecd5c8337
vboxuser <vboxuser@AESD-box-3.myguest.virtualbox.org>
2024-04-17 08:34:33 -0700

    Use github.com for test runner

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -10,7 +10,7 @@ AESD_ASSIGNMENTS_VERSION = 'e328d664f9eddb3bbae8fc936bfad77185bdda27'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = 'git@github-deftedge:cu-ecen-aeld/assignments-3-and-later-deftedge.git'
+AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-deftedge.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
